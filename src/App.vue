057dff906e1cddd0ab34ac600268c99034ce0713
barreto <antonio909790@yahoo.com.br>
2021-03-08 12:39:15 -0300

    image pannel style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="body">
+  <div>
     <h1 class="title">{{ titulo }}</h1>
 
     <ul class="picture-list">
@@ -13,6 +13,10 @@
 
       </li>
     </ul>
+
+    <footer>
+      <h4>Made with Vue.js by Gabriel Barreto 2021</h4>
+    </footer>
 
   </div>
 </template>
@@ -29,12 +33,12 @@ export default {
 
   data () {
     return {
-      titulo: 'Bem vindo ao RandomPics',
+      titulo: 'Nothing much, just a gallery of RandomPics',
       pictures: []
     }
   },
   created () {
-    let promise = this.$http.get('https://picsum.photos/v2/list?page=2&limit=100');
+    let promise = this.$http.get('https://picsum.photos/v2/list?page=2&limit=20');
     promise
     .then(res => res.json())
     .then(pictures => this.pictures = pictures, err => console.log(err)); // a resposta também é uma promise
@@ -42,18 +46,26 @@ export default {
 }
 </script>
 
-<style>
-.body {
+// scoped deixa os estilos afetarem apenas o component em que é declarado
+<style >
+
+body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin: 0 auto;
+  background-color: #8BC6EC;
+  background-image: linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%);
 }
 
 .title {
   text-align: center;
+  color: white;
+}
+
+footer {
+  color: white;
 }
 
 .picture-list {
--- src/components/shared/pannel/Pannel.vue
+++ src/components/shared/pannel/Pannel.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="pannel">
-    <h2 class="pannel-title">{{ title }}</h2>
-    <div class="pannel-content">
-      <img class="responsive-image" v-bind:src="url" :alt="author">
-      <span>Autor: {{ author }} </span>
-    </div>
+    <a :href="url">
+      <div class="pannel-content">
+        <img class="responsive-image" v-bind:src="url" :alt="author">
+        <p class="author-style">Autor: {{ author }} </p>
+      </div>
+    </a>
   </div>
 
 </template>
@@ -20,5 +21,39 @@ export default {
 
 <style>
 
+.pannel {
+  background-color: #D9AFD9;
+  background-image: linear-gradient(0deg, #D9AFD9 0%, #97D9E1 100%);
+  padding: 0 auto;
+  display: inline-block;
+  margin: 5px;
+  width: 200px;
+  height: 200px;
+  vertical-align: top;
+  text-align: center;
+  border-radius: 10px;
+}
+
+.responsive-image {
+  height: 150px;
+  width: 100%;
+  border-radius: 10px;
+}
+
+.author-style {
+  font-size: 13px;
+  font-weight: bold;
+}
+
+a {
+  color: #2c3e50;
+  text-decoration: none !important;
+}
+
+a:hover {
+    color:white;
+    text-decoration:none;
+    cursor:pointer;
+}
 
 </style>
